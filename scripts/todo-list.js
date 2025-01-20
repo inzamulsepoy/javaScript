ae0f7290b2fa661e6a3946755ad357f91ae4ad5d
inzamulsepoy <inzamulsepoygmail.com>
2025-01-20 21:04:40 +0530

    todo list

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,4 +1,45 @@
-const todoList = [];
+
+
+const todoList = [{
+  name: 'make dinner',
+  dueDate: '2022-12-22',
+},{
+  name: 'wash dishesh',
+  dueDate: '2022-12-22' 
+}
+];
+
+renderTodoList();
+
+function renderTodoList(){
+
+
+
+let todoListHTML = '';
+
+for(let i=0; i<todoList.length; i++){
+  const todoObject = todoList[i];
+  //const name = todoObject.name;
+  //const dueDate = todoObject.dueDate;
+  const {name, dueDate} = todoObject;   
+  const html = `
+    <p>
+      ${name} ${dueDate}
+      <button onclick="
+      
+      todoList.splice(${i},1);
+      renderTodoList();  
+      
+      ">Delete</button>
+    </p>`;
+  todoListHTML += html;
+  }
+  
+
+  document.querySelector('.js-todo-list')
+  .innerHTML = todoListHTML;
+
+  }
 
 function addTodo(){
 
@@ -6,10 +47,21 @@ const inputElement = document.querySelector('.js-name-input');
 
 const name = inputElement.value;
 
-todoList.push(name);
+const dateInputElement = document.querySelector('.js-due-date-input')
 
-console.log(todoList);
+const dueDate = dateInputElement.value;  
+
+todoList.push({
+ name: name, 
+ dueDate: dueDate
+    // name,
+    // dueDate
+});
+
+
 
 inputElement.value = '';
+
+renderTodoList();
 
 }
