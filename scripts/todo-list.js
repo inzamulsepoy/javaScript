754cca6142f1b028757ee4f31dc1bbcb5c0decd8
inzamulsepoy <inzamulsepoygmail.com>
2025-01-20 21:43:21 +0530

    todo list complete

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -23,15 +23,15 @@ for(let i=0; i<todoList.length; i++){
   //const dueDate = todoObject.dueDate;
   const {name, dueDate} = todoObject;   
   const html = `
-    <p>
-      ${name} ${dueDate}
-      <button onclick="
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
       
       todoList.splice(${i},1);
       renderTodoList();  
       
-      ">Delete</button>
-    </p>`;
+      " class="delete-todo-button">Delete</button>
+    `;
   todoListHTML += html;
   }
   
